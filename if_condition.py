c85dd41c35e514b00e87f5cf17acf0dde9ec2468
Amitha111 <83043214+Amitha111@users.noreply.github.com>
2021-04-28 20:30:27 -0500

    intialpush

---
+++ if_condition.py
@@ -0,0 +1,21 @@
+## If Condition
+var = "cherry"
+var1 = "apple"
+
+if var > var1: 
+    print ("Cherries are Bigger than Apples")
+
+if var < var1:
+    print ("Cherries are Smaller than Apples")
+
+if var == var1: 
+    print ("Cherries are Apples are Equal! ")
+
+
+
+l1 = ["cherries", "Apples", "Banana", "pineapple"]
+    
+if "cherries" in l1:
+    print ("Its a small fruit! ")
+if "carrot" not in l1:
+    print ("Its a vegetable")
